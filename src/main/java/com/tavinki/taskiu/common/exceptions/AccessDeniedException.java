c7f16a53e0ce0620c0345d6cd0ff1eda02cea2e1
VINCENT_PC\a1998 <vincenttam981@gmail.com>
2026-03-04 12:00:21 +0800

    feat: add AccessDeniedException for handling access control violations

---
+++ src/main/java/com/tavinki/taskiu/common/exceptions/AccessDeniedException.java
@@ -0,0 +1,11 @@
+package com.tavinki.taskiu.common.exceptions;
+
+public class AccessDeniedException extends RuntimeException {
+        public AccessDeniedException(String message) {
+        super(message);
+    }
+
+    public AccessDeniedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
