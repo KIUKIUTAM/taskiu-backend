86cc67aa6ba3aba3f8f1716607e947d2b13352f3
VINCENT_PC\a1998 <vincenttam981@gmail.com>
2026-03-05 17:17:40 +0800

    Revert "refactor: remove CSRF-disable protection from security configuration"
    This reverts commit 741de00bd11483a3ce92e89e158053ff5b50c37b.

--- src/main/java/com/tavinki/taskiu/common/config/SecurityConfig.java
+++ src/main/java/com/tavinki/taskiu/common/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(csrf -> csrf.disable())
                 .formLogin(form -> form.disable())
                 .httpBasic(basic -> basic.disable())
                 .authorizeHttpRequests(auth -> auth
